c83d80ab900e63fcd7ee579525923c69e9a2318c
Miranda Urban Solano <a01752391@tec.mx>
2025-03-18 20:09:12 -0600

    avance 18 marzo

--- ECG/Miranda/prueba.py
+++ ECG/Miranda/prueba.py
@@ -7,27 +7,42 @@ import numpy as np # Convertir datos
 from scipy.io import loadmat
 import numpy as np
 
-# Cargar el archivo y convertirlo
+# Cargar el archivo
 subject = "subject03.mat"
 file_path = "/Users/mirandaurbansolano/Documents/GitHub/" + subject
+
+# Convertir el archivo
 mat_data = loadmat(file_path) # Cargar el archivo .mat
 Cn = mat_data["Cn"] # Extraer la matriz Cn
 hrdata = np.hstack([np.array(x).flatten() for x in Cn.ravel()]).astype(np.float64)
 
-sample_rate = 256.0 # This set is sampled at 256Hz
+rate = 256.0  # La muestra esta sampleada a 256Hz
 
-# Returns two dictionaries: working data and measures
-working_data, measures = hp.process(hrdata, sample_rate) 
+# Regresa dos diccionarios: working data y measures
+working_data, measures = hp.process(hrdata, rate) 
+
+working_data, measures = hp.process_segmentwise(
+    hrdata,  
+    sample_rate = rate,  
+    segment_width = 15,  # 15 segundos  
+    segment_overlap=0.5,  
+    calc_freq=True,
+    reject_segmentwise=False,  # No eliminar segmentos
+    high_precision=True  # Más precisión  
+)
+
+print(f"Total peaks detected: {len(working_data['peaklist'])}")
+print(f"Peaks eliminados: {len(working_data['removed_beats'])}")
 
 # Plotter
-hp.plotter(working_data, measures, title = 'Ejemplo')
+#hp.plotter(working_data, measures, title = 'Ejemplo')
 
 # Save the graph with Matplotlib
-plt.savefig('plot_3.jpg')
+#plt.savefig('plot_3.jpg')
 
 # Print values
-print(f"BPM: {measures['bpm']}") # returns BPM value
-print(f"HRV Measure: {measures['rmssd']}") # returns RMSSD HRV measure
+#print(f"BPM: {measures['bpm']}") # returns BPM value
+#print(f"HRV Measure: {measures['rmssd']}") # returns RMSSD HRV measure
 
 # Show graph
-plt.show()
+#plt.show()
