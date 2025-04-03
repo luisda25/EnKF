4cef275437d9cce32136fb4203b992a8cca94b6a
Miranda Urban Solano <a01752391@tec.mx>
2025-04-03 10:44:40 -0600

    mas avances abril 3

--- ECG/Miranda/procesamientoDatos.py
+++ ECG/Miranda/procesamientoDatos.py
@@ -21,11 +21,10 @@ def analyze_data(subject, file_path):
     overlap = 0.5 # 
 
     # Interpolar datos
-    # time_original = np. linspace(0, len(hrdata)/sample_rate, len (hrdata)) # Crear un eje de tiempo original
-    # new_time = np. linspace(0, len(hrdata)/sample_rate, len(hrdata) * 2) # Crear un nuevo eje de tiempo con mayor resolución
-    # interpolator = interp1d(time_original, hrdata, kind='cubic')
-    # new_hrdata = interpolator(new_time)
-
+    #time_original = np. linspace(0, len(hrdata)/sample_rate, len (hrdata)) # Crear un eje de tiempo original
+    #new_time = np. linspace(0, len(hrdata)/sample_rate, len(hrdata) * 2) # Crear un nuevo eje de tiempo con mayor resolución
+    #interpolator = interp1d(time_original, hrdata, kind='cubic')
+    #new_hrdata = interpolator(new_time)
 
     # Procesar la señal
     working_data, measures = hp.process(hrdata, sample_rate) 
@@ -42,13 +41,31 @@ def analyze_data(subject, file_path):
     # Calcular los promedios de BPM y HRV
     bpm_avg = np.mean(measures['bpm'])
     hrv_avg = np.mean(measures['rmssd'])
+    lf_hf_avg = np.mean(measures['lf/hf'])
 
     # Imprimir los valores obtenidos
     print(f"Información de " + subject)
     print(f"Promedio de BPM: {bpm_avg:.2f}")
     print(f"Promedio de HRV (RMSSD): {hrv_avg:.2f}")
+    print(f"Promedio de lf/hf: {lf_hf_avg:.2f}")
 
-subject = "subject01"
+     # Imprimir los valores obtenidos
+    tit = "Datos de " + subject
+    bpm_text = "Promedio de BPM: " + str(round(bpm_avg, 2)) + "\n"
+    hrv_text = "Promedio de HRV (RMSSD): " + str(round(hrv_avg, 2)) + "\n"
+    lf_hf_text = "Promedio de lf/hf: " + str(round(lf_hf_avg, 2)) + "\n"
+
+    # Escribir en el archivo
+    resultados_sujeto = "resultados_" + subject + ".txt"
+    with open(resultados_sujeto, "a") as file:
+        file.write(tit + "\n")
+        file.write(bpm_text)
+        file.write(hrv_text)
+        file.write(lf_hf_text)
+        file.write("")
+
+
+subject = "subject04"
 subject_file = "/Users/mirandaurbansolano/Documents/GitHub/" + subject + ".mat"
 
 analyze_data(subject, subject_file)
